1f4d1c5e767c8f65193fc57d830a771407efbe41
zainmustafai <zforzain2000@gmail.com>
2023-08-24 14:15:31 -0700

    latest changes ooooffice

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
@@ -7,16 +6,13 @@ export const ThemeSwitcher = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
 
-
   useEffect(() => {
     setMounted(true);
   }, []);
 
-
   if (!mounted) {
     return null;
   }
-
 
   return (
     <button
@@ -24,7 +20,6 @@ export const ThemeSwitcher = () => {
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {theme === "light" ? "Dark" : "Light"}
-
     </button>
   );
 };
